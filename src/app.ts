2860dcb9037fe33e32153a28d3715ea9dde7691c
masaminh <masami.askw@gmail.com>
2021-01-05 20:06:42 +0900

    CORSを無効化

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,15 @@
 import getRaces from './get_races';
-
 import express = require('express');
 import moment = require('moment');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
 app.get('/v1/races', async (req, res) => {
   // TODO: パラメータチェック必要
-  const dateString = req.query.date as string
+  const dateString = req.query.date as string;
   const date = moment(dateString, ['YYYY-MM-DD']);
   const races = await getRaces(date);
   res.json(races);
 });
 
-module.exports = app
+module.exports = app;
