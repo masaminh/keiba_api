0d93f7fd7ef94f21a027719aa51810aaf51c7351
masaminh <masami.askw@gmail.com>
2021-01-16 15:44:53 +0900

    SonarCloud指摘の修正

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { DateTime } from 'luxon';
 import getRaces from './get_races';
 
 const app = express();
+app.disable('x-powered-by');
 
 app.get('/v1/races', async (req, res) => {
   // TODO: パラメータチェック必要
