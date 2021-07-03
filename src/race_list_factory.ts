b59d99104c3430a7b46a0e652c91c0fd358c6984
masaminh <33191610+masaminh@users.noreply.github.com>
2021-07-03 16:58:02 +0900

    引数名の変更誤りを訂正 (#22)

--- src/race_list_factory.ts
+++ src/race_list_factory.ts
@@ -4,15 +4,15 @@ import EntryRaceList from './entry_race_list';
 import ResultRaceList from './result_race_list';
 
 export default class {
-  public static create(raceid: string, $: cheerio.Root): RaceList {
+  public static create(key: string, $: cheerio.Root): RaceList {
     const header2 = $('.tbl-data-04 thead th:nth-of-type(2)').text();
 
     let raceList: RaceList;
 
     if (header2 === 'レース名') {
-      raceList = new ResultRaceList(raceid, $);
+      raceList = new ResultRaceList(key, $);
     } else {
-      raceList = new EntryRaceList(raceid, $);
+      raceList = new EntryRaceList(key, $);
     }
 
     return raceList;
