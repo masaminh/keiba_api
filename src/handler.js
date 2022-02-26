3e0751f806c7b8b26284b40b346e50219b140189
masaminh <masami.askw@gmail.com>
2022-02-26 20:46:14 +0900

    ライブラリ、Nodeのバージョンアップ

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./app');
 
-const server = serverlessExpress.createServer(app);
-
-exports.handler = async (event, context) => {
-  return serverlessExpress.proxy(server, event, context, 'PROMISE').promise;
-}
+exports.handler = serverlessExpress({ app });
