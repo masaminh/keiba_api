e36ee934c7cf66f89f31291a4077f5cc40cc2953
masaminh <masami.askw@gmail.com>
2022-02-27 19:43:06 +0900

    コーディングスタイルの整理

--- src/entry_race_detail.test.ts
+++ src/entry_race_detail.test.ts
@@ -5,16 +5,26 @@ import EntryRaceDetail from './entry_race_detail';
 describe('EntryRaceDetail', (): void => {
   test.each`
     message         | raceid             | file                                          | expectedRace                                                                                              | targetHorsePos | expectedHorse
-    ${'馬番発表前'} | ${'2021062010511'} | ${'testdata/EntryRaceDetail_getRaceDetail_1'} | ${{ id: '2021062010511', courseid: '105', coursename: '東京', racenumber: 11, racename: 'ユニコーンＳ' }} | ${0}           | ${{ bracketnumber: undefined, horsenumber: undefined, horsename: 'イグナイター', horseid: '0001261958' }}
-    ${'馬番発表後'} | ${'2021062710911'} | ${'testdata/EntryRaceDetail_getRaceDetail_2'} | ${{ id: '2021062710911', courseid: '109', coursename: '阪神', racenumber: 11, racename: '宝塚記念' }}     | ${0}           | ${{ bracketnumber: 1, horsenumber: 1, horsename: 'ユニコーンライオン', horseid: '0001295861' }}
+    ${'馬番発表前'} | ${'2021062010511'} | ${'testdata/EntryRaceDetail_getRaceDetail_1'} | ${{
+  id: '2021062010511', courseid: '105', coursename: '東京', racenumber: 11, racename: 'ユニコーンＳ',
+}} | ${0}           | ${{
+  bracketnumber: undefined, horsenumber: undefined, horsename: 'イグナイター', horseid: '0001261958',
+}}
+    ${'馬番発表後'} | ${'2021062710911'} | ${'testdata/EntryRaceDetail_getRaceDetail_2'} | ${{
+  id: '2021062710911', courseid: '109', coursename: '阪神', racenumber: 11, racename: '宝塚記念',
+}}     | ${0}           | ${{
+  bracketnumber: 1, horsenumber: 1, horsename: 'ユニコーンライオン', horseid: '0001295861',
+}}
   `(
     'getRaceDetail ($message)',
-    ({ raceid, file, expectedRace, targetHorsePos, expectedHorse }) => {
+    ({
+      raceid, file, expectedRace, targetHorsePos, expectedHorse,
+    }) => {
       const body = fs.readFileSync(file, 'utf-8');
 
       const raceDetailInstance = new EntryRaceDetail(
         raceid,
-        cheerio.load(body)
+        cheerio.load(body),
       );
       const raceDetail = raceDetailInstance.getRaceDetail();
       expect(raceDetail.raceinfo).toEqual(expectedRace);
@@ -22,6 +32,6 @@ describe('EntryRaceDetail', (): void => {
       if (raceDetail.horses != null) {
         expect(raceDetail.horses[targetHorsePos]).toEqual(expectedHorse);
       }
-    }
+    },
   );
 });
--- src/get_racedetail.test.ts
+++ src/get_racedetail.test.ts
@@ -5,9 +5,15 @@ import Storage from './storage';
 describe('get_racedetail', () => {
   test.each`
     key                                | file                                 | expected                                                                                                  | expectedNum | message
-    ${'jbis/race/20210620/105/11.htm'} | ${'testdata/GetRaceDetail_Entry_1'}  | ${{ id: '2021062010511', courseid: '105', coursename: '東京', racenumber: 11, racename: 'ユニコーンＳ' }} | ${16}       | ${'Race Entry'}
-    ${'jbis/race/20210620/105/11.htm'} | ${'testdata/GetRaceDetail_Result_1'} | ${{ id: '2021062010511', courseid: '105', coursename: '東京', racenumber: 11, racename: 'ユニコーンＳ' }} | ${16}       | ${'Race Result'}
-  `('$message', async ({ key, file, expected, expectedNum }) => {
+    ${'jbis/race/20210620/105/11.htm'} | ${'testdata/GetRaceDetail_Entry_1'}  | ${{
+  id: '2021062010511', courseid: '105', coursename: '東京', racenumber: 11, racename: 'ユニコーンＳ',
+}} | ${16}       | ${'Race Entry'}
+    ${'jbis/race/20210620/105/11.htm'} | ${'testdata/GetRaceDetail_Result_1'} | ${{
+  id: '2021062010511', courseid: '105', coursename: '東京', racenumber: 11, racename: 'ユニコーンＳ',
+}} | ${16}       | ${'Race Result'}
+  `('$message', async ({
+    key, file, expected, expectedNum,
+  }) => {
     Storage.getKeys = jest.fn().mockResolvedValue([key]);
 
     const body = fs.readFileSync(file, 'utf-8');
