1fe7c4dbc28bb439ff8f5f5f1ec89d9be7c56f7c
masaminh <masami.askw@gmail.com>
2023-01-05 11:53:15 +0900

    同着があった日のraces APIがレース番号0のレース情報を返す問題の対応

--- src/result_race_list.test.ts
+++ src/result_race_list.test.ts
@@ -17,4 +17,14 @@ describe('ResultRaceList', (): void => {
       racenumber: 11,
     });
   });
+
+  test('getRaces (同着あり)', () => {
+    const key = 'jbis/race/calendar/20221207/220';
+    const body = fs.readFileSync('testdata/ResultRaceList_getRaces_2', 'utf-8');
+    const raceList = new ResultRaceList(key, cheerio.load(body));
+    const races = raceList.getRaces();
+    expect(races).toHaveLength(12);
+    const badRaceInfo = races.filter((r) => (r.racename === '' || r.racenumber === 0));
+    expect(badRaceInfo).toHaveLength(0);
+  });
 });
--- src/result_race_list.ts
+++ src/result_race_list.ts
@@ -3,7 +3,16 @@ import RaceList from './race_list';
 
 export default class extends RaceList {
   protected getRaceNumber(elm: cheerio.Element): number {
-    return Number(this.$('th', elm).text());
+    const raceNumberString = this.$('th', elm).text();
+
+    if (raceNumberString === '') {
+      // thタグがない時、raceNumberStringは空文字列になっている。
+      // 同着があった時、レース内2行目はこの状態になるため、
+      // 読み飛ばさせるために、NaNを返す。
+      return NaN;
+    }
+
+    return Number(raceNumberString);
   }
 
   protected getRaceName(elm: cheerio.Element): string {
